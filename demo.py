1c4d0b2d2b9d93e565b86e86473e862ba84caedc
mayuanchi <mayuanchi1029@gmail.com>
2024-09-06 15:02:07 +0800

    perf: 增加实时数据

---
+++ demo.py
@@ -0,0 +1,66 @@
+import backtrader as bt
+from qmtbt import QMTStore
+from datetime import datetime
+
+store = QMTStore()
+
+class DualMovingAverageStrategy(bt.Strategy):
+    params = (
+        ('short_window', 10),
+        ('long_window', 30),
+    )
+
+    def __init__(self):
+        self.short_ma = bt.indicators.MovingAverageSimple(
+            self.data.close, period=self.params.short_window
+        )
+        self.long_ma = bt.indicators.MovingAverageSimple(
+            self.data.close, period=self.params.long_window
+        )
+
+    def next(self):
+        print('next', len(self.data.close))
+        if self.short_ma > self.long_ma:
+            if not self.position:
+                self.buy()
+        elif self.short_ma < self.long_ma:
+            if self.position:
+                self.sell()
+
+    def notify_data(self, data, status, *args, **kwargs):
+        print(self.data.close[0], 'self.data.close')
+        # if status == bt.dataseries.DATASERIES_EVENT_UPDATE:
+            # 数据更新事件触发时执行的逻辑
+        # print(data)
+
+if __name__ == '__main__':
+    cerebro = bt.Cerebro()
+
+    # 添加数据
+    data = store.getdata(dataname='600900.SH', timeframe=bt.TimeFrame.Days, fromdate=datetime(2022, 7, 1), dividend_type='front', live=True, cerebro=cerebro)
+    cerebro.adddata(data)
+
+    # 添加策略
+    cerebro.addstrategy(DualMovingAverageStrategy)
+
+    cerebro.optstrategy
+
+    # 设置初始资金
+    cerebro.broker.setcash(100000.0)
+
+    # 设置佣金
+    cerebro.broker.setcommission(commission=0.001)
+
+    # 运行回测
+    print('Starting Portfolio Value: %.2f' % cerebro.broker.getvalue())
+    cerebro.run()
+    print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+
+    # data.test(1)
+    # data.test(2)
+    # data.test(3)
+    # data.test(4)
+    # xtdata.run()
+
+    # 绘制结果
+    # cerebro.plot()
--- qmtbt/qmtfeed.py
+++ qmtbt/qmtfeed.py
@@ -3,9 +3,12 @@ from __future__ import (absolute_import, division, print_function,
                         unicode_literals)
 
 from collections import deque
-from datetime import datetime
+import datetime
 import backtrader as bt
 from backtrader.feed import DataBase
+import time
+import threading
+import random
 
 from .qmtstore import QMTStore
 
@@ -26,7 +29,7 @@ class QMTFeed(DataBase, metaclass=MetaQMTFeed):
       - ``historical`` (default: ``False``)
     """
 
-    lines = ('lastClose', 'amount', 'pvolume', 'stockStatus', 'openInt', 'lastSettlementPrice', 'settlementPrice', 'transactionNum', 'askPrice1', 'askPrice2', 'askPrice3', 'askPrice4', 'askPrice5', 'bidPrice1', 'bidPrice2', 'bidPrice3', 'bidPrice4', 'bidPrice5', 'askVol1', 'askVol2', 'askVol3', 'askVol4', 'askVol5', 'bidVol1', 'bidVol2', 'bidVol3', 'bidVol4', 'bidVol5', )
+    lines = ('lastClose', 'amount', 'pvolume', 'stockStatus', 'openInt', 'lastSettlementPrice', 'settlementPrice', 'transactionNum', 'askPrice1', 'askPrice2', 'askPrice3', 'askPrice4', 'askPrice5', 'bidPrice1', 'bidPrice2', 'bidPrice3', 'bidPrice4', 'bidPrice5', 'askVol1', 'askVol2', 'askVol3', 'askVol4', 'askVol5', 'bidVol1', 'bidVol2', 'bidVol3', 'bidVol4', 'bidVol5', 'openInterest', 'dr', 'totaldr', 'preClose', 'suspendFlag', 'settelementPrice' )
 
     params = (
         ('live', False),  # only historical download
@@ -37,6 +40,7 @@ class QMTFeed(DataBase, metaclass=MetaQMTFeed):
         self._timeframe = self.p.timeframe
         self._compression = 1
         self.store = kwargs['store']
+        self.cerebro = kwargs['cerebro']
         self._data = deque()  # data queue for price data
         self._seq = None
 
@@ -46,16 +50,15 @@ class QMTFeed(DataBase, metaclass=MetaQMTFeed):
         period_map = {
             bt.TimeFrame.Days: '1d',
             bt.TimeFrame.Minutes: '1m',
-            bt.TimeFrame.Ticks: 'tick'
+            # bt.TimeFrame.Ticks: 'tick'
         } 
 
         if not self.p.live:
             self._history_data(period=period_map[self.p.timeframe])
             print(f'{self.p.dataname}历史数据装载成功！')
         else:
-            self._history_data(period=period_map[self.p.timeframe])
+            self._live_data(period=period_map[self.p.timeframe])
             print(f'{self.p.dataname}实时数据装载成功！')
-            # self._live_data(period=period_map[self.p.timeframe])
 
     def stop(self):
         DataBase.stop(self)
@@ -63,24 +66,34 @@ class QMTFeed(DataBase, metaclass=MetaQMTFeed):
         if self.p.live:
             self.store._unsubscribe_live(self._seq)
 
-    def _load(self):
-        while len(self._data):
+    def _get_datetime(self, value):
+        dtime = datetime.datetime.fromtimestamp(value // 1000)
+        return bt.date2num(dtime)
 
-            current = self._data.popleft()
-
-            for key in current.keys():   
+    def _load_current(self, current):
+        for key in current.keys():   
                 try: 
                     value = current[key]
                     if key == 'time':
-                        dtime = datetime.fromtimestamp(value // 1000)
-                        self.lines.datetime[0] = bt.date2num(dtime)
+                        self.lines.datetime[0] = self._get_datetime(value)
                     elif key == 'lastPrice' and self.p.timeframe == bt.TimeFrame.Ticks:
                         self.lines.close[0] = value
                     else:
                         attr = getattr(self.lines, key)
                         attr[0] = value
-                except:
+                except Exception as e:
+                    print(e)
                     pass
+        # print(current, 'current')
+        self.put_notification(int(random.randint(100000, 999999)))
+
+    def _load(self, replace=False):
+        if len(self._data) > 0:
+
+            current = self._data.popleft()
+
+            self._load_current(current)
+
             return True
         return None
     
@@ -115,8 +128,50 @@ class QMTFeed(DataBase, metaclass=MetaQMTFeed):
 
     def _live_data(self, period):
 
+        start_time = self._format_datetime(self.p.fromdate, period)
+
         def on_data(res):
-            self._data.append(res.iloc[0].to_dict())
+            current = res[self.p.dataname][0]
+            if self._get_datetime(current['time']) == self.lines.datetime[0]:
+                self._load_current(current)
+            else:
+                self._data.append(current)
 
 
-        self._seq = self.store._subscribe_live(symbol=self.p.dataname, period=period, callback=on_data)
+        self._seq = self.store._subscribe_live(symbol=self.p.dataname, period=period, start_time=start_time, callback=on_data)
+
+        res = self.store._fetch_history(symbol=self.p.dataname, period=period, start_time=start_time)
+        result = res.to_dict('records')
+        for item in result:
+            self._data.append(item)
+
+    # def test(self, close):
+    #     # 获取当前日期
+    #     current_date = datetime.datetime.now().date()
+
+    #     # 将当前日期转换为datetime对象，并将时间设置为00:00:00
+    #     start_of_day = datetime.datetime.combine(current_date, datetime.time.min)
+
+    #     # 将开始时间转换为Unix时间戳（以毫秒为单位）
+    #     start_of_day_unix_ms = int(start_of_day.timestamp() * 1000)
+    #     current = {
+    #         'time': start_of_day_unix_ms,
+    #         'close': close,
+    #         'open': 1.0,
+    #         'high': 1.0,
+    #         'low': 1.0,
+    #         'volume': 1.0,
+    #     }
+    #     self._load_current(current)
+
+    # # 定义一个函数，这个函数将在每次循环中执行
+    # def my_function():
+    #     while True:
+    #         self.test(1)
+    #         time.sleep(3)  # 暂停3秒
+
+    # # 创建并启动线程
+    # thread = threading.Thread(target=my_function)
+    # thread.daemon = True  # 设置为守护线程，这样主线程结束时，子线程也会结束
+    # thread.start()
+
